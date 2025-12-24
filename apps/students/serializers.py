ffbc88fdd5fee0a925226db0e311b9aeb7ff1dc1
rizqya-adzra <rizqyaadzrazahirasudrajat@smkwikrama.sch.id>
2025-12-24 08:50:20 +0700

    fix: added name for rayon, rombel, jurusan to StudentSerializer

--- apps/students/serializers.py
+++ apps/students/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from .models import Student, Jurusan, Rayon, Rombel
+        
+class JurusanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Jurusan
+        fields = ['id', 'name']
 
+
+class RayonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rayon
+        fields = ['id', 'name']
+
+
+class RombelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rombel
+        fields = ['id', 'tingkatan']
 
 class StudentSerializer(serializers.ModelSerializer):
+    jurusan = JurusanSerializer(read_only=True)
+    rayon = RayonSerializer(read_only=True)
+    rombel = RombelSerializer(read_only=True)
     class Meta:
         model = Student
         fields = [
@@ -17,18 +36,3 @@ class StudentSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
-        
-class JurusanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Jurusan
-        fields = '__all__'
-
-class RayonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rayon
-        fields = '__all__'
-
-class RombelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rombel
-        fields = '__all__'
